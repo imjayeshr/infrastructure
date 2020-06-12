94a6f9c266987c02540277e18cc022953df3302f
Jayesh Raghuwanshi <55672780+imjayeshr@users.noreply.github.com>
2020-06-11 23:58:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,13 @@ resource "aws_route_table" "csye6225_route_table" {
 
   
 }
+resource "aws_route" "csye6225_route" {
 
+  route_table_id = "${aws_route_table.csye6225_route_table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+
+}
 resource "aws_route_table_association" "csye6225_route_table_association" {
   count = "${length(var.subnet_cidrs)}"
 
