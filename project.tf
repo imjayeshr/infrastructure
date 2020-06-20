b58056bb1bfeaaf3c8a2a885478381cb5a8aea4d
Jayesh Raghuwanshi <jayesh.raghuwanshi2207@gmail.com>
2020-06-20 15:31:04 -0400

    Modifying project.tf and variables.tf

--- project.tf
+++ project.tf
@@ -1,14 +1,14 @@
 provider "aws"{
-    region = "${var.region}"
+    region              = "${var.region}"
 }
 
 //CREATING VPC FOR INSTANCE
 resource "aws_vpc" "csye6225_awsdev"{
-    cidr_block  = "10.0.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
-    enable_classiclink_dns_support = true
-    assign_generated_ipv6_cidr_block = false
+    cidr_block                          = "10.0.0.0/16"
+    enable_dns_hostnames                = true
+    enable_dns_support                  = true
+    enable_classiclink_dns_support      = true
+    assign_generated_ipv6_cidr_block    = false
 
     tags={
         Name = "csye6225_awsdev"
@@ -17,15 +17,16 @@ resource "aws_vpc" "csye6225_awsdev"{
 
 //INTERNET GATEWAY
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = "${aws_vpc.csye6225_awsdev.id}"
+  vpc_id            = "${aws_vpc.csye6225_awsdev.id}"
 
   tags ={
-      Name = "Internet_Gateway"
+      Name          = "Internet_Gateway"
   }
 }
 
 //CREATING SUBNETS
 resource "aws_subnet" "csye6225_subnet"{
+
     count                   = "${length(data.aws_availability_zones.availability_zones.names)}"
     vpc_id                  = "${aws_vpc.csye6225_awsdev.id}"
     //cidr_block              = "10.0.${count.index}.0/24"
@@ -38,14 +39,15 @@ resource "aws_subnet" "csye6225_subnet"{
 }
 //CREATING ROUTE TABLE
 resource "aws_route_table" "csye6225_route_table" {
-  vpc_id = "${aws_vpc.csye6225_awsdev.id}"
+
+  vpc_id            = "${aws_vpc.csye6225_awsdev.id}"
 }
 //CREATING ROUTE
 resource "aws_route" "csye6225_route" {
 
-  route_table_id = "${aws_route_table.csye6225_route_table.id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+  route_table_id                = "${aws_route_table.csye6225_route_table.id}"
+  destination_cidr_block        = "0.0.0.0/0"
+  gateway_id                    = "${aws_internet_gateway.internet_gateway.id}"
 
 }
 //CREATING ROUTE TABLE ASSOCIATION
@@ -59,109 +61,113 @@ resource "aws_route_table_association" "csye6225_route_table_association" {
 //CREATING APP SECURITY GROUP   ---> FROM ANYWHERE IN THE WORLD
 resource "aws_security_group" "application"{
 
-    name        = "application"
-    description = "App Security Group"
-    vpc_id      = "${aws_vpc.csye6225_awsdev.id}" 
+    name                = "application"
+    description         = "App Security Group"
+    vpc_id              = "${aws_vpc.csye6225_awsdev.id}" 
 
     ingress {
         description = "TLS from VPC"
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-        cidr_blocks = [aws_vpc.csye6225_awsdev.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         description = "HTTP REQUEST from VPC"
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = [aws_vpc.csye6225_awsdev.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         description = "SSH from VPC"
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = [aws_vpc.csye6225_awsdev.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
   ingress {
         description = "FOR NODE REQUEST "
         from_port   = 3301
         to_port     = 3301
         protocol    = "tcp"
-        cidr_blocks = [aws_vpc.csye6225_awsdev.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
   ingress {
         description = "FOR ANGULAR PORT"
         from_port   = 4200
         to_port     = 4200
         protocol    = "tcp"
-        cidr_blocks = [aws_vpc.csye6225_awsdev.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
     }
+    egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 //CREATING DB SECURITY GROUP   ------> SOURCE OF TRAFFIC SHOUDL BE APPLICATION SECURITY GROUP
 resource "aws_security_group" "database"{
 
-    name        = "database"
-    description = "DB Security Group"
-    vpc_id      = "${aws_vpc.csye6225_awsdev.id}" 
+    name                    = "database"
+    description             = "DB Security Group"
+    vpc_id                  = "${aws_vpc.csye6225_awsdev.id}" 
 
     ingress {
         description = "FOR MYSQL REQUEST "
         from_port   = 3306
         to_port     = 3306
         protocol    = "tcp"
-        cidr_blocks = [aws_vpc.csye6225_awsdev.cidr_block]
+        cidr_blocks = ["0.0.0.0/0"]
         security_groups = ["${aws_security_group.application.id}"]
-        //security_groups = [aws_security_group.application_data.id]
+        
     }
+    egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
-# resource "aws_network_interface" "networkInterface" {
-#     count                   = "${length(data.aws_availability_zones.availability_zones.names)}"  
-#     subnet_id               = "${element(aws_subnet.csye6225_subnet.*.id, count.index)}"
-#     //subnet_id           = ["${aws_subnet.csye6225_subnet.id}"]
-#     security_groups     = ["${aws_security_group.application.id}","${aws_security_group.database.id}"]
-
-   
-
-# }
 
 resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
+  description                       = "This key is used to encrypt bucket objects"
+  deletion_window_in_days           = 10
 }
 
 //CREATING S3 BUCKET
 resource "aws_s3_bucket" "S3_Bucket" {
-    bucket = "webapp.jayesh.raghuwanshi"
-    acl= "private"
-    force_destroy = true
+    bucket          = "${var.bucket_name}"
+    acl             = "private"
+    force_destroy   = true
 
 
     lifecycle_rule {
         enabled = true
         transition {
-        days = 30
-        storage_class = "STANDARD_IA"
+            days = 30
+            storage_class = "STANDARD_IA"
         }
     }
     server_side_encryption_configuration {
         rule {
         apply_server_side_encryption_by_default {
-            kms_master_key_id = "${aws_kms_key.mykey.arn}"
-            sse_algorithm     = "aws:kms"
+            //kms_master_key_id = "${aws_kms_key.mykey.arn}"
+            sse_algorithm     = "AES256"
         }
         }
     }
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
-  bucket = "${aws_s3_bucket.S3_Bucket.id}"
 
-  block_public_acls   = true
-  block_public_policy = false
+        bucket = "${aws_s3_bucket.S3_Bucket.id}"
+
+        block_public_acls   = true
+        block_public_policy = false
 }
 
 //CREATING RDS BD SUBNET GROUP
@@ -179,24 +185,29 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 
 //CREATING RDS INSTANCE
 resource "aws_db_instance" "rds_instance"{
-    allocated_storage    = "20"
+    allocated_storage           = "20"
     
-    engine                 = "mysql"
-    instance_class         = "db.t3.micro"
-    multi_az               = false
-    identifier             = "csye6225su2020"
-    username               = "csye6225su2020"
-    password               = "jayesh2207"
-    vpc_security_group_ids = ["${aws_security_group.database.id}"]
-    db_subnet_group_name   = "${aws_db_subnet_group.db_subnet_group.id}"
-    publicly_accessible    = "false"
-    name                   = "csye6225"
+    engine                      = "${var.db_engine}"
+    instance_class              = "db.t3.micro"
+    multi_az                    = false
+    identifier                  = "${var.db_instance_identifier}"
+    username                    = "${var.db_username}"
+    password                    = "${var.db_password}"
+    vpc_security_group_ids      = ["${aws_security_group.database.id}"]
+    db_subnet_group_name        = "${aws_db_subnet_group.db_subnet_group.id}"
+    publicly_accessible         = "false"
+    name                        = "${var.db_name}"
+
+    //final_snapshot_identifier   = true
+    skip_final_snapshot         = true
+    deletion_protection         = false
+    delete_automated_backups    = true
 
 }
 
 //CREATING DYNAMO_DB TABLE
 resource "aws_dynamodb_table" "dynamodb_table" {
-    name           = "csye6225"
+    name           = "${var.dynamodb_table_name}"
     
     billing_mode   = "PROVISIONED"
     read_capacity  = 20
@@ -258,26 +269,42 @@ resource "aws_iam_role_policy" "iam_role_policy" {
 EOF
 }
 
+
 resource "aws_iam_instance_profile" "EC2-CSYE6225" {
   name = "EC2-CSYE6225"
   role = "${aws_iam_role.iam_role.name}"
 }
+data "aws_db_instance" "databaseData" {
+  db_instance_identifier = "${aws_db_instance.rds_instance.id}"
+ }
+
+
+data "template_file" "init" {
+  template = "${file("init.tpl")}"
+  vars = {
+    consul_address = "${data.aws_db_instance.databaseData.address}"
+  }
+}
 
 //CREATING EC2 INSTANCE  ------> take variable from command line
 resource "aws_instance" "web"{
-    ami                             = "ami-0ab7f75e6820dd728"
+    ami                             = "${var.ami_id}"
     count                           = "${length(data.aws_availability_zones.availability_zones.names)}"  
     subnet_id                       = "${element(aws_subnet.csye6225_subnet.*.id, count.index)}"
     associate_public_ip_address     = true
     instance_type                   = "t2.micro"
-    disable_api_termination         = true
+    //disable_api_termination         = true
     iam_instance_profile            = "${aws_iam_instance_profile.EC2-CSYE6225.name}"
     vpc_security_group_ids          = ["${aws_security_group.application.id}"]
+    key_name                        = "${var.ssh-key-name}"
     
+    user_data                       =     "${file("init.tpl")}"
+
+
     root_block_device {
     volume_size           = "20"
     volume_type           = "gp2"
-    //delete_on_termination = "${var.EC2_ROOT_VOLUME_DELETE_ON_TERMINATION}"
+    delete_on_termination = true
   }
 }
 
